fb077e8e2fcb8f75f07652fe0ce8d1d7d5ea213e
9wan9hyeon <54222988+kimgwang-hyeon@users.noreply.github.com>
2026-07-31 12:29:21 +0900

    study: v-html and XSS

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
 import SampleOne from './components/practices/basic/SampleOne.vue'
 import SampleTwo from './components/practices/basic/SampleTwo.vue'
+import VueHtml from './components/practices/basic/VueHtml.vue'
+import VueHtmlXSS from './components/practices/basic/VueHtmlXSS.vue'
 </script>
 
 <template>
   <div style="padding: 20px">
     <SampleOne />
     <SampleTwo />
+    <VueHtml />
+    <VueHtmlXSS />
   </div>
 </template>
