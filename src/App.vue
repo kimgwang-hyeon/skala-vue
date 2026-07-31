a67270404d00465e1fe6c017d110501d204bb903
9wan9hyeon <54222988+kimgwang-hyeon@users.noreply.github.com>
2026-07-31 11:59:14 +0900

    Initial commit + sample one, two

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import SampleOne from './components/practices/basic/SampleOne.vue'
+import SampleTwo from './components/practices/basic/SampleTwo.vue'
+</script>
+
+<template>
+  <div style="padding: 20px">
+    <SampleOne />
+    <SampleTwo />
+  </div>
+</template>
---
+++ src/components/practices/basic/SampleOne.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+
+// 1. 일반 변수 (화면이 실시간으로 바뀌지 않음)
+let normalCount = 0
+
+// 2. 반응성 변수(화면이 실시간으로 바뀜)
+const vueCount = ref(0)
+</script>
+
+<template>
+  <div>
+    <h2>Hello Skala-Vue</h2>
+    <h3>일반 변수 클릭: {{ normalCount }}</h3>
+    <button @click="normalCount++">일반 변수 증가</button>
+    <br />
+    <br />
+    <h3>반응성 변수 클릭: {{ vueCount }}</h3>
+    <button @click="vueCount++">반응성 변수 증가</button>
+  </div>
+</template>
